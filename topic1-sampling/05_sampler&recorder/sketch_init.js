ced1dc31d7ff47bc5072d3f3023adfee1c743788
David Schornsheim <david.schornsheim@gmx.us>
2018-07-12 13:06:32 +0200

    The big one

--- topic1-sampling/05_sampler&recorder/sketch_init.js
+++ topic1-sampling/05_sampler&recorder/sketch_init.js
@@ -6,7 +6,7 @@
 // sampler of four sounds (keys 'a', 'w', 'd', 'x')
 // recorder (keys 'spacebar' to record/stop recording/reset recording, and 'm' to play the recorded sound
 
-var sound1, sound2, sound3, sound4;
+var sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16, sound17;
 var isLooping;
 var bgvalue = 0;
 
@@ -16,6 +16,19 @@ function preload(){
   sound2 = loadSound("sounds/psi_35.ogg", loaded);
   sound3 = loadSound("sounds/SFX_Jump_10.ogg", loaded);
   sound4 = loadSound("sounds/Synth Bass 016.ogg", loaded);
+  sound5 = loadSound("../../sounds/Braaaam.ogg", loaded);
+  sound6 = loadSound("../../sounds/burp.wav", loaded);
+  sound7 = loadSound("../../sounds/br0kenviolin-001.wav", loaded);
+  sound8 = loadSound("../../sounds/br0kenviolin-002.wav", loaded);
+  sound9 = loadSound("../../sounds/br0kenviolin-003.wav", loaded);
+  sound10 = loadSound("../../sounds/synth-snare.ogg", loaded);
+  sound11 = loadSound("../../sounds/synth-swoosh.ogg", loaded);
+  sound12 = loadSound("../../sounds/synth-transformers.ogg", loaded);
+  sound13 = loadSound("../../sounds/cymbal.ogg", loaded);
+  sound14 = loadSound("../../sounds/explosion.ogg", loaded);
+  sound15 = loadSound("../../sounds/hihat.ogg", loaded);
+  sound16 = loadSound("../../sounds/monkey.ogg", loaded);
+  sound17 = loadSound("../../sounds/snare.ogg", loaded);
 }
 
 function setup(){
@@ -36,21 +49,47 @@ function loaded() {
 }
 
 function keyPressed() {
-  if (keyCode == 65) {
+    if (keyCode === 65) {
         console.log("a");
         sound1.play();
         bgvalue = random(1, 255);
-    } else if (keyCode == 87) {
+    } else if (keyCode === 87) {
         console.log("w");
         sound2.play();
         bgvalue = random(1, 255);
-    } else if (keyCode == 68) {
+    } else if (keyCode === 68) {
         console.log("d");
         sound3.play();
         bgvalue = random(1, 255);
-    } else if (keyCode == 88) {
+    } else if (keyCode === 88) {
         console.log("x");
         sound4.play();
         bgvalue = random(1, 255);
+    } else if (keyCode === 66) {
+        sound5.play();
+    } else if (keyCode === 67) {
+        sound6.play();
+    } else if (keyCode === 77) {
+        sound7.play();
+    } else if (keyCode === 69) {
+        sound8.play();
+    } else if (keyCode === 70) {
+        sound9.play();
+    } else if (keyCode === 71) {
+        sound10.play();
+    } else if (keyCode === 71) {
+        sound11.play();
+    } else if (keyCode === 72) {
+        sound12.play();
+    } else if (keyCode === 73) {
+        sound13.play();
+    } else if (keyCode === 74) {
+        sound14.play();
+    } else if (keyCode === 75) {
+        sound15.play();
+    } else if (keyCode === 76) {
+        sound16.play();
+    } else if (keyCode === 77) {
+        sound17.play();
     }
 }
