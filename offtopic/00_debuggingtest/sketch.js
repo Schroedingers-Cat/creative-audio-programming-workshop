d0cf0399c3fef90c59b96388709d68b8e21e9fc0
David Schornsheim <david.schornsheim@gmx.us>
2018-07-12 10:32:03 +0200

    Getting started

--- offtopic/00_debuggingtest/sketch.js
+++ offtopic/00_debuggingtest/sketch.js
@@ -1,8 +1,12 @@
 // define vars here
 // print some vars directly in the console
 
+var playing;
+
 function setup() {
   // put setup code here
+    console.log("hello you nice people on the world <3");
+    playing = false;
   // print some values in the console
 }
 
