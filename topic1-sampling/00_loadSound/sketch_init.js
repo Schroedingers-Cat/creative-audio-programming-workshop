503a0762a2170cece2917b4f0707e45888760802
David Schornsheim <david.schornsheim@gmx.us>
2018-07-12 10:40:56 +0200

    PlaySound - it is really simple <3

--- topic1-sampling/00_loadSound/sketch_init.js
+++ topic1-sampling/00_loadSound/sketch_init.js
@@ -8,14 +8,19 @@ var sound;
 
 function preload(){
   // load sound here
+    sound = loadSound("sounds/Prassel_noise_loop_4s.ogg", success());
 }
 
 function setup(){
   createCanvas(200, 200);
   // play or loop sound here
-
+    sound.play();
 }
 
 function draw(){
   background(0);
+}
+
+function success() {
+    console.log("The rocket has launched.");
 }
